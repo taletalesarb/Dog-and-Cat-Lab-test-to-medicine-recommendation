fd8bd5a14ecb39436731d71c85891a0cecddf98f
ming <thanawit321@hotmail.com>
2020-08-17 14:01:40 +0700

    Add new part

---
+++ Test_deploy_web/deploy_vue/src/lab_table/Lab_table.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <table class="tg">
+      <thead>
+        <tr>
+          <th class="tg-title" rowspan="2">Antimicrobials</th>
+          <th class="tg-title" colspan="2">Results</th>
+        </tr>
+        <tr>
+          <td class="tg-title">MIC</td>
+          <td class="tg-title">S/I/R</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="value in med_lists" :key="value">
+          <td class="tg-med">{{value.med_name}}</td>
+          <td class="tg-MIC">>=320</td>
+          <td class="tg-SIR">{{value.SIR}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "lab_table",
+  data() {
+    return {};
+  },
+  props: {
+    med_lists: Array
+  },
+  mounted() {
+    console.log("-->>med_lists");
+    console.log(med_lists);
+  }
+};
+</script>
+
+<style type="text/css">
+.tg {
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 400px;
+}
+.tg td {
+  border-color: black;
+  border-style: solid;
+  border-width: 1px;
+  font-family: Arial, sans-serif;
+  font-size: 14px;
+  overflow: hidden;
+  padding: 10px 5px;
+  word-break: normal;
+}
+.tg th {
+  border-color: black;
+  border-style: solid;
+  border-width: 1px;
+  font-family: Arial, sans-serif;
+  font-size: 14px;
+  font-weight: normal;
+  overflow: hidden;
+  padding: 10px 5px;
+  word-break: normal;
+}
+.tg .tg-med {
+  text-align: left;
+  vertical-align: center;
+  width: 500px;
+  max-width: 500px;
+}
+.tg .tg-MIC {
+  text-align: center;
+  vertical-align: center;
+  width: 40px;
+}
+.tg .tg-SIR {
+  text-align: center;
+  vertical-align: center;
+  width: 40px;
+}
+.tg .tg-title {
+  text-align: center;
+  vertical-align: center;
+}
+</style>
---
+++ Test_deploy_web/deploy_vue/src/recommendation/Recommendation.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="recommendation">
+      <div class="rec-title">
+        <p>4. Recommendation of antimicrobial choices</p>
+      </div>
+      <div class="rec-detail">
+        <p>Bacterial Species identification</p>
+        <p>- จากการทำ urine quantitative culture บ่งชี้ว่าเป็น ...</p>
+        <p>- ยาต้านจุลชีพที่แนะนำ ตามลำดับ ได้แก่</p>
+        <div class="rec-med">
+          <div class="get-med">
+            <!-- get med recommendation from api -->
+          </div>
+          <p>
+            หรือให้ยาต่อเนื่องจนอาการหายสนิท และให้
+            ยาต่อไปอีก 1-2 สัปดาห์
+          </p>
+        </div>
+        <p>แนะนำให้ flush ล้างกระเพาะปัสสาวะด้วย NSS จนปัสสาวะใส เพื่อช่วยกำจัด/ลดจำนวนเชื้อในกระเพาะปัสสาวะ</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "recommendation"
+};
+</script>
+
+<style>
+.recommendation {
+  color: grey;
+  margin-top: -520px;
+}
+.rec-detail {
+  margin-left: 20px;
+}
+.rec-med {
+  margin-left: 20px;
+}
+</style>
