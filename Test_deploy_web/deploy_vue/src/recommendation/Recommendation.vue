fe50225e1f74ae38bee0e8f9be4808d2695c8fe7
ming <thanawit321@hotmail.com>
2020-09-08 20:55:55 +0700

    update recommendation.vue

--- Test_deploy_web/deploy_vue/src/recommendation/Recommendation.vue
+++ Test_deploy_web/deploy_vue/src/recommendation/Recommendation.vue
@@ -6,18 +6,23 @@
       </div>
       <div class="rec-detail">
         <p>Bacterial Species identification</p>
-        <p>- จากการทำ urine quantitative culture บ่งชี้ว่าเป็น ...</p>
+        <!-- <p>- จากการทำ urine quantitative culture บ่งชี้ว่าเป็น ...</p> -->
         <p>- ยาต้านจุลชีพที่แนะนำ ตามลำดับ ได้แก่</p>
         <div class="rec-med">
           <div class="get-med">
             <!-- get med recommendation from api -->
+            <p v-for="(med, index) in med_recommend" :key="'med' + index">
+              {{ med }}
+            </p>
           </div>
-          <p>
-            หรือให้ยาต่อเนื่องจนอาการหายสนิท และให้
-            ยาต่อไปอีก 1-2 สัปดาห์
-          </p>
+          <!-- <p>
+            หรือให้ยาต่อเนื่องจนอาการหายสนิท และให้ ยาต่อไปอีก 1-2 สัปดาห์
+          </p> -->
         </div>
-        <p>แนะนำให้ flush ล้างกระเพาะปัสสาวะด้วย NSS จนปัสสาวะใส เพื่อช่วยกำจัด/ลดจำนวนเชื้อในกระเพาะปัสสาวะ</p>
+        <!-- <p>
+          แนะนำให้ flush ล้างกระเพาะปัสสาวะด้วย NSS จนปัสสาวะใส
+          เพื่อช่วยกำจัด/ลดจำนวนเชื้อในกระเพาะปัสสาวะ
+        </p> -->
       </div>
     </div>
   </div>
@@ -26,6 +31,9 @@
 <script>
 export default {
   name: "recommendation",
+  props: {
+    med_recommend: Array
+  }
 };
 </script>
 
